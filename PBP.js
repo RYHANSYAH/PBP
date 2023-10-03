1d03b04166ef233da6a1faf350241b38c4242f18
RAYHANSYAH <114573108+RYHANSYAH@users.noreply.github.com>
2023-10-03 23:04:01 +0700

    Add files via upload

---
+++ PBP.js
@@ -0,0 +1,6 @@
+var firstName = "RAYHAN"; 
+var lastName = "SYAH";  
+var age = 20;   
+var isMarried = false; 
+
+console.log("Hallo,Nama Saya "+firstName+lastName,",Umur Saya baru menginjak "+age, "tahun")
